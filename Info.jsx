5e75e7c6e290a8c4c697fd1e2ad6a4e4bb3566f2
Abhi19mar <64063769+Abhi19mar@users.noreply.github.com>
2021-06-28 00:36:36 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return(
+    <div className= "note">
+      <h1> Javascript and React.js </h1>
+      <p> I loved the bootcamp. Everything was taught brilliantly </p>
+    </div>
+  );
+
+}
+
+export default Info;
